cf2048264df0f1f87749165afe6380c71f1b556d
Drewry Pope <drewrypope@gmail.com>
2020-07-18 05:06:27 -0500

    improve twitter-tweet

---
+++ bin/server.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+/* Copyright 2020 Drewry Pope
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import {} from 'dotenv/config.js' ;
+import { tweet } from 'twitter-tweet' ;
+const params = ((inputSubdomain = 'api') => {
+  const {
+    subdomain ,
+    consumer_key ,
+    consumer_secret ,
+    access_token_key ,
+    access_token_secret
+  } = process.env ;
+  return {
+    subdomain : subdomain || inputSubdomain ,
+    consumer_key ,
+    consumer_secret ,
+    access_token_key ,
+    access_token_secret
+  } ;
+})() ;
+(async () => {
+  try {
+    await tweet.send(
+      {process.argv[2] || process.env.TWITTER_CONTENT || "HELLO WORLD, 'AT' " + new Date().toISOString() ,
+      process.argv[3] || "" ,
+      params
+    ) ;
+  } catch (error) { throw error }
+})().catch(e => {console.error(e)}) ;
