1665f626e7865d5383ecdfc96315a47de989bea1
Drewry Pope <drewrypope@gmail.com>
2020-07-20 22:38:21 -0500

    minor changes

--- bin/server.js
+++ bin/server.js
@@ -4,42 +4,40 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import {} from 'dotenv/config.js' ;
-import { tweet } from 'twitter-tweet' ;
+import { tweet } from '../lib/index.js' ;
 const params = ((inputSubdomain = 'api') => {
-  const {
-    subdomain ,
-    consumer_key ,
-    consumer_secret ,
-    access_token_key ,
-    access_token_secret
-  } = process.env ;
-  return {
-    subdomain : subdomain || inputSubdomain ,
-    consumer_key ,
-    consumer_secret ,
-    access_token_key ,
-    access_token_secret
-  } ;
+    const {
+        subdomain ,
+        consumer_key ,
+        consumer_secret ,
+        access_token_key ,
+        access_token_secret
+    } = process.env ;
+    return {
+        subdomain : subdomain || inputSubdomain ,
+        consumer_key ,
+        consumer_secret ,
+        access_token_key ,
+        access_token_secret
+    } ;
 })() ;
 const args = process.argv ;
 console.log(args) ;
 args.shift() ;
 args.shift() ;
 let id = null;
-if (args[0].toString().trim() == "r" || args[0].toString().trim() == "-r" || args[0].toString().trim() == "/r") {
-  args.shift() ;
-  id = args[0] ;
-  args.shift() ;
-  console.log(JSON.stringify([id])) ;
+if (args[0].toString().trim() == 'r' || args[0].toString().trim() == '-r' || args[0].toString().trim() == '/r') {
+    args.shift() ;
+    id = args[0] ;
+    args.shift() ;
+    console.log(JSON.stringify([id])) ;
 }
-const content = [JSON.parse(JSON.stringify([args || process.env.TWITTER_CONTENT || "HELLO WORLD, 'AT' " + new Date().toISOString()]))].flat(Infinity) ;
-console.log(JSON.stringify(content)) ;
+const thread = [JSON.parse(JSON.stringify([args || process.env.TWITTER_CONTENT || 'HELLO WORLD, \'AT\' ' + new Date().toISOString()]))].flat(Infinity) ;
+console.log(JSON.stringify(thread)) ;
 (async () => {
-  try {
     await tweet.send(
-      content ,
-      id ,
-      params
+        params ,
+        thread ,
+        id
     ) ;
-  } catch (error) { throw error }
-})().catch(e => {console.error(e)}) ;
+})().catch(e => {console.error(e);}) ;
